7246999ab4429230c20ed27df6bf5e211eebabe2
Arthur20111 <123042130+Arthur20111@users.noreply.github.com>
2023-05-09 20:25:06 -0300

    a
    a

---
+++ main.js
@@ -0,0 +1,41 @@
+var noseX = 0;
+var noseY = 0;
+var clown_nose;
+var canvas;
+var video;
+
+function preload() {
+    clown_nose = loadImage('cat.png')
+}
+
+function setup() {
+    canvas = createCanvas(301, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(301, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized')
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x - 90;
+        noseY = results[0].pose.nose.y - 95;
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX, noseY, 180, 180);
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
